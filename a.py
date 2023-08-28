4872a27f83e3140e97f7e533062c1b89ce43f9a0
Настя <vasyushkinaan@mail.ru>
2023-08-28 21:05:21 +0500

    Третий комит

--- a.py
+++ a.py
@@ -1,1 +1,12 @@
-print('baby jungwonie')
+def palindrom(word):
+    first = []
+    second = []
+    for i in word:
+        first.append(i)
+    for a in reversed(word):
+        second.append(a)
+    if first == second:
+        print(True)
+    else:
+        print(False)
+palindrom('шалаш')
